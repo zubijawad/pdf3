a2f215e19343e69d0db9e6f0e3cf74e5eee7fd03
zubijawad <zubijawad495@gmail.com>
2024-12-22 06:11:07 -0800

    Add files via upload

---
+++ zubi/html/app.js
@@ -0,0 +1,14 @@
+// answaer number 1
+alert("javascript alert")
+// answer number 2
+alert("Error!please enter a volid password")
+// answer number 3
+alert("welcome to js land...\n happy coding")
+// answer number 4
+alert("welcome to js land...")
+alert("Happy coding!\n prevent this page from creating adtional dialogs")
+// answer number 5
+console.log("Hello... i can run js through my web browser's console")
+// answer number 6
+alert("html")
+alert("css")
